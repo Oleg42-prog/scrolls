2771f9a700426f8523a33361cdbe615fe59b0342
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-05 13:24:04 +0300

    Add first_frame

--- computer-vision/utils.py
+++ computer-vision/utils.py
@@ -4,3 +4,10 @@ import cv2
 def compress_twice(img):
     h, w, _ = img.shape
     return cv2.resize(img, (w // 2, h // 2))
+
+
+def first_frame(file_path):
+    cap = cv2.VideoCapture(file_path)
+    ret, frame = cap.read()
+    cap.release()
+    return ret, frame
