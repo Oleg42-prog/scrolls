8e110679956f2a51922c6412cbebf2832116ddcc
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-05 14:50:25 +0300

    Add binary_mask

--- computer-vision/utils.py
+++ computer-vision/utils.py
@@ -11,3 +11,14 @@ def first_frame(file_path):
     ret, frame = cap.read()
     cap.release()
     return ret, frame
+
+
+def binary_mask(image):
+    _image = image.copy().astype('uint8')
+    _image[_image > 0] = 1
+    _image[_image <= 0] = 0
+    return _image.astype('uint8')
+
+
+def binary_mask_255(image):
+    return binary_mask(image) * 255
