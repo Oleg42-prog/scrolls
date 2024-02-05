12fdc98f7ca78b3e1eb90c51a56720b280a18d1e
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-05 15:32:49 +0300

    Add filter_viewer

---
+++ computer-vision/filter_viewer.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def filter_viewer(video_capture_input, image_filter, windows_name='main', wait_key_delay=1):
+
+    cap = cv2.VideoCapture(video_capture_input)
+
+    while True:
+        ret, image = cap.read()
+
+        if ret is not True:
+            break
+
+        cv2.imshow(windows_name, image_filter(image))
+
+        if cv2.waitKey(wait_key_delay) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
