918dba7ea0b0ec638d4e4e8aad353fac541c752c
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-05 17:54:51 +0300

    Create a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+   name='scrolls',
+   version='0.1',
+   description='Useful solutions/templates/patterns',
+   author='Oleg Dudnik',
+   author_email='Oleggelo86@gmail.com',
+   packages=find_packages(include=['scrolls', 'scrolls.*']),
+   install_requires=['opencv-python'],
+)
