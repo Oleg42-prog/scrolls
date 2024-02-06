1c1a53888644586debf1f5edceb902e2eb3b377a
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-06 13:54:34 +0300

    Simplify hsv trackbar

--- scrolls/cv/hsv_trackbar.py
+++ scrolls/cv/hsv_trackbar.py
@@ -5,45 +5,22 @@ import numpy as np
 class HSVTrackbar:
 
     def __init__(self, window_name):
+        self.colors = 'hsv'
         self.window_name = window_name
+
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
-        cv2.createTrackbar('lower h', window_name, 0, 255, lambda x: x)
-        cv2.createTrackbar('lower s', window_name, 0, 255, lambda x: x)
-        cv2.createTrackbar('lower v', window_name, 0, 255, lambda x: x)
+        for color in self.colors:
+            cv2.createTrackbar(f'lower {color}', window_name, 0, 255, lambda x: x)
+            cv2.createTrackbar(f'upper {color}', window_name, 0, 255, lambda x: x)
 
-        cv2.createTrackbar('upper h', window_name, 0, 255, lambda x: x)
-        cv2.createTrackbar('upper s', window_name, 0, 255, lambda x: x)
-        cv2.createTrackbar('upper v', window_name, 0, 255, lambda x: x)
-
-    @property
-    def lower_h(self):
-        return cv2.getTrackbarPos('lower h', self.window_name)
-
-    @property
-    def lower_s(self):
-        return cv2.getTrackbarPos('lower s', self.window_name)
-
-    @property
-    def lower_v(self):
-        return cv2.getTrackbarPos('lower v', self.window_name)
+    def get(self, bound, color):
+        return cv2.getTrackbarPos(f'{bound} {color}', self.window_name)
 
     @property
     def lower(self):
-        return np.array([self.lower_h, self.lower_s, self.lower_v])
-
-    @property
-    def upper_h(self):
-        return cv2.getTrackbarPos('upper h', self.window_name)
-
-    @property
-    def upper_s(self):
-        return cv2.getTrackbarPos('upper s', self.window_name)
-
-    @property
-    def upper_v(self):
-        return cv2.getTrackbarPos('upper v', self.window_name)
+        return np.array([self.get('lower', color) for color in self.colors])
 
     @property
     def upper(self):
-        return np.array([self.upper_h, self.upper_s, self.upper_v])
+        return np.array([self.get('upper', color) for color in self.colors])
