19ec4ae75b2628d68ca0b1103bee1c7ebac6059f
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-06 14:28:34 +0300

    Abstract trackbar

--- scrolls/cv/bounds_trackbar.py
+++ scrolls/cv/bounds_trackbar.py
@@ -1,19 +1,60 @@
 import cv2
+import numpy as np
 
 
-class BoundsTrackbar:
+class Trackbar:
 
-    def __init__(self, window_name, init_lower=0, init_upper=255, max_value=255):
+    def __init__(self, window_name, trackbar_name, init_value=0, max_value=255):
         self.window_name = window_name
-        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        self.trackbar_name = trackbar_name
 
-        cv2.createTrackbar('lower', self.window_name, init_lower, max_value, lambda x: x)
-        cv2.createTrackbar('upper', self.window_name, init_upper, max_value, lambda x: x)
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        cv2.createTrackbar(trackbar_name, window_name, init_value, max_value, lambda x: x)
+
+    @property
+    def value(self):
+        return cv2.getTrackbarPos(self.trackbar_name, self.window_name)
+
+
+class BoundaryTrackbars:
+
+    def __init__(self, window_name, init_lower=0, init_upper=255, max_value=255, lower_name='lower', upper_name='upper'):
+        self.lower_trackbar = Trackbar(window_name, lower_name, init_value=init_lower, max_value=max_value)
+        self.upper_trackbar = Trackbar(window_name, upper_name, init_value=init_upper, max_value=max_value)
 
     @property
     def lower(self):
-        return cv2.getTrackbarPos('lower', self.window_name)
+        return self.lower_trackbar.value
 
     @property
     def upper(self):
-        return cv2.getTrackbarPos('upper', self.window_name)
+        return self.upper_trackbar.value
+
+
+class ColorBoundaryTrackbars:
+
+    def __init__(self, window_name, colors):
+        self.window_name = window_name
+        self.colors = colors
+
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+        for color in colors:
+            cv2.createTrackbar(f'lower {color}', window_name, 0, 255, lambda x: x)
+            cv2.createTrackbar(f'upper {color}', window_name, 0, 255, lambda x: x)
+
+    def get_lower(self, name):
+        return cv2.getTrackbarPos(f'lower {name}', self.window_name)
+
+    @property
+    def lower(self):
+        return np.array([self.get_lower(color) for color in self.colors])
+
+    def get_upper(self, name):
+        return cv2.getTrackbarPos(f'upper {name}', self.window_name)
+
+    @property
+    def upper(self):
+        return np.array([self.get_upper(color) for color in self.colors])
+
+
