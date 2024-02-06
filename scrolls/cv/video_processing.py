5684c48367e403f6f2f1b6c57bdd774c579bf0e6
Oleg Dudnik <oleggelo86@gmail.com>
2024-02-06 17:50:35 +0300

    Add frames reader / writer

--- scrolls/cv/video_processing.py
+++ scrolls/cv/video_processing.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 
 
@@ -6,3 +7,18 @@ def first_frame(file_path):
     ret, frame = cap.read()
     cap.release()
     return ret, frame
+
+
+def frames_reader(video_file_path):
+    cap = cv2.VideoCapture(video_file_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        yield frame
+
+
+def frames_writer(folder, frames_generator):
+    for frame_number, frame in enumerate(frames_generator):
+        file_path = os.path.join(folder, f'{frame_number:04d}.png')
+        cv2.imwrite(file_path, frame)
