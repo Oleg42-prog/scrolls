ed67ab1ebc2602e7291d8a608079418292cb1635
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-22 13:00:24 +0300

    Make ColorBoundaryTrackbar and HSVTrackbar consistent

--- scrolls/cv/trackbars.py
+++ scrolls/cv/trackbars.py
@@ -76,19 +76,22 @@ class ColorBoundaryTrackbars:
 
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
-        for color in colors:
+        for color in self.colors:
             cv2.createTrackbar(f'lower {color}', window_name, 0, 255, lambda x: x)
-            cv2.createTrackbar(f'upper {color}', window_name, 0, 255, lambda x: x)
+            cv2.createTrackbar(f'upper {color}', window_name, 255, 255, lambda x: x)
 
-    def get_lower(self, name):
-        return cv2.getTrackbarPos(f'lower {name}', self.window_name)
+    def get(self, bound, color):
+        return cv2.getTrackbarPos(f'{bound} {color}', self.window_name)
+
+    def get_lower(self, color):
+        return self.get('lower', color)
+
+    def get_upper(self, color):
+        return self.get('upper', color)
 
     @property
     def lower(self):
         return np.array([self.get_lower(color) for color in self.colors])
-
-    def get_upper(self, name):
-        return cv2.getTrackbarPos(f'upper {name}', self.window_name)
 
     @property
     def upper(self):
@@ -98,8 +101,8 @@ class ColorBoundaryTrackbars:
 class HSVTrackbar:
 
     def __init__(self, window_name):
-        self.colors = 'hsv'
         self.window_name = window_name
+        self.colors = 'hsv'
 
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
@@ -110,10 +113,16 @@ class HSVTrackbar:
     def get(self, bound, color):
         return cv2.getTrackbarPos(f'{bound} {color}', self.window_name)
 
+    def get_lower(self, color):
+        return self.get('lower', color)
+
+    def get_upper(self, color):
+        return self.get('upper', color)
+
     @property
     def lower(self):
-        return np.array([self.get('lower', color) for color in self.colors])
+        return np.array([self.get_lower(color) for color in self.colors])
 
     @property
     def upper(self):
-        return np.array([self.get('upper', color) for color in self.colors])
+        return np.array([self.get_upper(color) for color in self.colors])
