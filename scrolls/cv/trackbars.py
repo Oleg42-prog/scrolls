61459a7e1a01b41986e7e26ef0b9923dc8e63a04
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-22 13:03:46 +0300

    Inherit HSVTrackbars from ColorTrackbars

--- scrolls/cv/trackbars.py
+++ scrolls/cv/trackbars.py
@@ -98,31 +98,7 @@ class ColorTrackbars:
         return np.array([self.get_upper(color) for color in self.colors])
 
 
-class HSVTrackbars:
+class HSVTrackbars(ColorTrackbars):
 
     def __init__(self, window_name):
-        self.window_name = window_name
-        self.colors = 'hsv'
-
-        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-
-        for color in self.colors:
-            cv2.createTrackbar(f'lower {color}', window_name, 0, 255, lambda x: x)
-            cv2.createTrackbar(f'upper {color}', window_name, 255, 255, lambda x: x)
-
-    def get(self, bound, color):
-        return cv2.getTrackbarPos(f'{bound} {color}', self.window_name)
-
-    def get_lower(self, color):
-        return self.get('lower', color)
-
-    def get_upper(self, color):
-        return self.get('upper', color)
-
-    @property
-    def lower(self):
-        return np.array([self.get_lower(color) for color in self.colors])
-
-    @property
-    def upper(self):
-        return np.array([self.get_upper(color) for color in self.colors])
+        super().__init__(window_name, 'hsv')
