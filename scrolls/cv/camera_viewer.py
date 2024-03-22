8638d18c2d29b74d2f0e9eb8f26b059020589cec
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-22 12:20:30 +0300

    Cover camera_viewer.py into function

--- scrolls/cv/camera_viewer.py
+++ scrolls/cv/camera_viewer.py
@@ -1,14 +1,28 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # rtsp://login:pass@ip
 
-while True:
-    ret, frame = cap.read()
+def camera_viewer(
+    video_capture_input,
+    windows_name='main',
+    wait_key_delay=1,
+):
 
-    cv2.imshow('frame', frame)
+    cap = cv2.VideoCapture(video_capture_input)  # rtsp://login:pass@ip
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    while True:
+        ret, frame = cap.read()
 
-cap.release()
-cv2.destroyAllWindows()
+        if not ret:
+            break
+
+        cv2.imshow(windows_name, frame)
+
+        if cv2.waitKey(wait_key_delay) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    camera_viewer(0)
