6fd1135a14913ab2a6bc9f8dc9d3472a5b2c37d3
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-26 17:29:11 +0300

    Add zoom argument

--- scrolls/plot/tsne.py
+++ scrolls/plot/tsne.py
@@ -5,10 +5,10 @@ from sklearn.manifold import TSNE
 from scrolls.cv.image_processing import everything_to_pil_image
 
 
-def show_tsne(x, y, selected_filenames):
+def show_tsne(x, y, selected_filenames, zoom=1):
     fig, axis = plt.subplots()
     fig.set_size_inches(22, 22, forward=True)
-    plot_images_in_2d(x, y, selected_filenames, zoom=0.3, axis=axis)
+    plot_images_in_2d(x, y, selected_filenames, zoom=zoom, axis=axis)
     plt.show()
 
 
@@ -25,7 +25,7 @@ def plot_images_in_2d(x, y, images, axis=None, zoom=1):
     axis.autoscale()
 
 
-def tsne_to_grid_plotter_manual(x, y, selected_filenames):
+def tsne_to_grid_plotter_manual(x, y, selected_filenames, zoom=1):
     S = 2000
     s = 100
     x = (x - min(x)) / (max(x) - min(x))
@@ -47,26 +47,27 @@ def tsne_to_grid_plotter_manual(x, y, selected_filenames):
         filename_plot.append(image_path)
     fig, axis = plt.subplots()
     fig.set_size_inches(22, 22, forward=True)
-    plot_images_in_2d(x_values, y_values, filename_plot, zoom=.58, axis=axis)
+    plot_images_in_2d(x_values, y_values, filename_plot, zoom=zoom, axis=axis)
     plt.show()
 
 
 class TSNEVisualizer:
 
-    def __init__(self, features, images_input) -> None:
+    def __init__(self, features, images_input, zoom=1) -> None:
         self._tsne_results = TSNE(
             n_components=2,
             verbose=1,
             metric='euclidean'
         ).fit_transform(features)
         self._images = list(map(everything_to_pil_image, images_input))
+        self.zoom = zoom
 
     def plot(self):
         plt.scatter(self._tsne_results[:, 0], self._tsne_results[:, 1])
         plt.show()
 
     def plot_icons(self):
-        show_tsne(self._tsne_results[:, 0], self._tsne_results[:, 1], self._images)
+        show_tsne(self._tsne_results[:, 0], self._tsne_results[:, 1], self._images, zoom=self.zoom)
 
     def plot_icons_grid(self):
-        tsne_to_grid_plotter_manual(self._tsne_results[:, 0], self._tsne_results[:, 1], self._images)
+        tsne_to_grid_plotter_manual(self._tsne_results[:, 0], self._tsne_results[:, 1], self._images, zoom=self.zoom)
