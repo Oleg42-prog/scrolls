45668684e8a72f2caa99425f215ca88443862c7f
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-26 17:09:06 +0300

    Update package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='scrolls',
-   version='0.1.9',
+   version='0.1.9.1',
    description='Useful solutions/templates/patterns',
    author='Oleg Dudnik',
    author_email='Oleggelo86@gmail.com',
