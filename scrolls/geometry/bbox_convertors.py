9af78caa287091df4af6c1cce12889d91eab888f
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 16:39:23 +0300

    Add boxes_cxywh_to_cxywhn

--- scrolls/geometry/bbox_convertors.py
+++ scrolls/geometry/bbox_convertors.py
@@ -39,3 +39,7 @@ def boxes_xyxy_to_xyxyn(bounding_boxes, image_size):
 
 def boxes_xywh_to_xywhn(bounding_boxes, image_size):
     return normalize_bounding_boxes(bounding_boxes, image_size)
+
+
+def boxes_cxywh_to_cxywhn(bounding_boxes, image_size):
+    return normalize_bounding_boxes(bounding_boxes, image_size)
