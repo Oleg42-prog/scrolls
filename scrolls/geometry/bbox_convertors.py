26f5fc45bb21210aeb7a6ee20cd3f4674d738b32
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 17:09:00 +0300

    Add to percentage functions

--- scrolls/geometry/bbox_convertors.py
+++ scrolls/geometry/bbox_convertors.py
@@ -43,3 +43,15 @@ def boxes_xywh_to_xywhn(bounding_boxes, image_size):
 
 def boxes_cxywh_to_cxywhn(bounding_boxes, image_size):
     return normalize_bounding_boxes(bounding_boxes, image_size)
+
+
+def boxes_xyxy_to_xyxyp(bounding_boxes, image_size):
+    return boxes_xyxy_to_xyxyn(bounding_boxes, image_size) * 100
+
+
+def boxes_xywh_to_xywhp(bounding_boxes, image_size):
+    return boxes_xywh_to_xywhn(bounding_boxes, image_size) * 100
+
+
+def boxes_cxywh_to_cxywhp(bounding_boxes, image_size):
+    return boxes_cxywh_to_cxywhn(bounding_boxes, image_size) * 100
