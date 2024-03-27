104da47ea01dafbf5f1075db620ff7fa974c48ab
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 17:17:29 +0300

    Add from normalised functions

--- scrolls/geometry/bbox_convertors.py
+++ scrolls/geometry/bbox_convertors.py
@@ -8,6 +8,12 @@ def normalize_bounding_boxes(bounding_boxes, image_size):
     return bounding_boxes / transformation_vector
 
 
+def rescale_bounding_boxes(bounding_boxes, image_size):
+    image_width, image_height = image_size
+    transformation_vector = np.array([image_width, image_height, image_width, image_height])
+    return bounding_boxes * transformation_vector
+
+
 def box_xyxy_to_xywh(bounding_box):
     bounding_boxes = bounding_box.reshape(1, -1)
     return boxes_xyxy_to_xywh(bounding_boxes)
@@ -43,6 +49,18 @@ def boxes_xywh_to_xywhn(bounding_boxes, image_size):
 
 def boxes_cxywh_to_cxywhn(bounding_boxes, image_size):
     return normalize_bounding_boxes(bounding_boxes, image_size)
+
+
+def boxes_xyxyn_to_xyxy(bounding_boxes, image_size):
+    return rescale_bounding_boxes(bounding_boxes, image_size)
+
+
+def boxes_xywhn_to_xywh(bounding_boxes, image_size):
+    return rescale_bounding_boxes(bounding_boxes, image_size)
+
+
+def boxes_cxywhn_to_cxywh(bounding_boxes, image_size):
+    return rescale_bounding_boxes(bounding_boxes, image_size)
 
 
 def boxes_xyxy_to_xyxyp(bounding_boxes, image_size):
