0fa765795cc64c4812de4a83c1ec0723b9a29fff
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 18:56:08 +0300

    Add identity functions implementations to bbox_convertors_all.py

--- scrolls/geometry/bbox_convertors_all.py
+++ scrolls/geometry/bbox_convertors_all.py
@@ -1,251 +1,331 @@
 # xyxy
-def xyxy_to_xyxy():
-    pass
+def xyxy_to_xyxy(bounding_boxes):
+    return bounding_boxes
+
 
 def xyxy_to_xywh():
     pass
 
+
 def xyxy_to_xyxyn():
     pass
+
 
 def xyxy_to_xywhn():
     pass
 
+
 def xyxy_to_cxywh():
     pass
+
 
 def xyxy_to_cxywhn():
     pass
 
+
 def xyxy_to_xyxyp():
     pass
+
 
 def xyxy_to_xywhp():
     pass
 
+
 def xyxy_to_cxywhp():
     pass
+
 
 # xywh
 def xywh_to_xyxy():
     pass
 
-def xywh_to_xywh():
-    pass
+
+def xywh_to_xywh(bounding_boxes):
+    return bounding_boxes
+
 
 def xywh_to_xyxyn():
     pass
 
+
 def xywh_to_xywhn():
     pass
+
 
 def xywh_to_cxywh():
     pass
 
+
 def xywh_to_cxywhn():
     pass
+
 
 def xywh_to_xyxyp():
     pass
 
+
 def xywh_to_xywhp():
     pass
 
+
 def xywh_to_cxywhp():
     pass
+
 
 # xyxyn
 def xyxyn_to_xyxy():
     pass
 
+
 def xyxyn_to_xywh():
     pass
 
-def xyxyn_to_xyxyn():
-    pass
+
+def xyxyn_to_xyxyn(bounding_boxes):
+    return bounding_boxes
+
 
 def xyxyn_to_xywhn():
     pass
 
+
 def xyxyn_to_cxywh():
     pass
+
 
 def xyxyn_to_cxywhn():
     pass
 
+
 def xyxyn_to_xyxyp():
     pass
+
 
 def xyxyn_to_xywhp():
     pass
 
+
 def xyxyn_to_cxywhp():
     pass
+
 
 # xywhn
 def xywhn_to_xyxy():
     pass
 
+
 def xywhn_to_xywh():
     pass
+
 
 def xywhn_to_xyxyn():
     pass
 
-def xywhn_to_xywhn():
-    pass
+
+def xywhn_to_xywhn(bounding_boxes):
+    return bounding_boxes
+
 
 def xywhn_to_cxywh():
     pass
 
+
 def xywhn_to_cxywhn():
     pass
+
 
 def xywhn_to_xyxyp():
     pass
 
+
 def xywhn_to_xywhp():
     pass
 
+
 def xywhn_to_cxywhp():
     pass
+
 
 # cxywh
 def cxywh_to_xyxy():
     pass
 
+
 def cxywh_to_xywh():
     pass
+
 
 def cxywh_to_xyxyn():
     pass
 
+
 def cxywh_to_xywhn():
     pass
 
-def cxywh_to_cxywh():
-    pass
+
+def cxywh_to_cxywh(bounding_boxes):
+    return bounding_boxes
+
 
 def cxywh_to_cxywhn():
     pass
 
+
 def cxywh_to_xyxyp():
     pass
+
 
 def cxywh_to_xywhp():
     pass
 
+
 def cxywh_to_cxywhp():
     pass
+
 
 # cxywhn
 def cxywhn_to_xyxy():
     pass
 
+
 def cxywhn_to_xywh():
     pass
+
 
 def cxywhn_to_xyxyn():
     pass
 
+
 def cxywhn_to_xywhn():
     pass
+
 
 def cxywhn_to_cxywh():
     pass
 
-def cxywhn_to_cxywhn():
-    pass
+
+def cxywhn_to_cxywhn(bounding_boxes):
+    return bounding_boxes
+
 
 def cxywhn_to_xyxyp():
     pass
 
+
 def cxywhn_to_xywhp():
     pass
 
+
 def cxywhn_to_cxywhp():
     pass
+
 
 # xyxyp
 def xyxyp_to_xyxy():
     pass
 
+
 def xyxyp_to_xywh():
     pass
+
 
 def xyxyp_to_xyxyn():
     pass
 
+
 def xyxyp_to_xywhn():
     pass
+
 
 def xyxyp_to_cxywh():
     pass
 
+
 def xyxyp_to_cxywhn():
     pass
 
-def xyxyp_to_xyxyp():
-    pass
+
+def xyxyp_to_xyxyp(bounding_boxes):
+    return bounding_boxes
+
 
 def xyxyp_to_xywhp():
     pass
 
+
 def xyxyp_to_cxywhp():
     pass
+
 
 # xywhp
 def xywhp_to_xyxy():
     pass
 
+
 def xywhp_to_xywh():
     pass
+
 
 def xywhp_to_xyxyn():
     pass
 
+
 def xywhp_to_xywhn():
     pass
+
 
 def xywhp_to_cxywh():
     pass
 
+
 def xywhp_to_cxywhn():
     pass
+
 
 def xywhp_to_xyxyp():
     pass
 
-def xywhp_to_xywhp():
-    pass
+
+def xywhp_to_xywhp(bounding_boxes):
+    return bounding_boxes
+
 
 def xywhp_to_cxywhp():
     pass
+
 
 # cxywhp
 def cxywhp_to_xyxy():
     pass
 
+
 def cxywhp_to_xywh():
     pass
+
 
 def cxywhp_to_xyxyn():
     pass
 
+
 def cxywhp_to_xywhn():
     pass
+
 
 def cxywhp_to_cxywh():
     pass
 
+
 def cxywhp_to_cxywhn():
     pass
+
 
 def cxywhp_to_xyxyp():
     pass
 
+
 def cxywhp_to_xywhp():
     pass
 
-def cxywhp_to_cxywhp():
-    pass
+
+def cxywhp_to_cxywhp(bounding_boxes):
+    return bounding_boxes
