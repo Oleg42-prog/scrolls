f32c6e190796cc6271d73b2596ec4ccbe01c9d9a
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 16:36:09 +0300

    Abstract normalize_bounding_boxes function

--- scrolls/geometry/bbox_convertors.py
+++ scrolls/geometry/bbox_convertors.py
@@ -2,6 +2,12 @@ import numpy as np
 from scrolls.geometry.linal import apply_linear_operator
 
 
+def normalize_bounding_boxes(bounding_boxes, image_size):
+    image_width, image_height = image_size
+    transformation_vector = np.array([image_width, image_height, image_width, image_height])
+    return bounding_boxes / transformation_vector
+
+
 def box_xyxy_to_xywh(bounding_box):
     bounding_boxes = bounding_box.reshape(1, -1)
     return boxes_xyxy_to_xywh(bounding_boxes)
@@ -28,6 +34,4 @@ def boxes_xywh_to_xyxy(bounding_boxes):
 
 
 def boxes_xyxy_to_xyxyn(bounding_boxes, image_size):
-    image_width, image_height = image_size
-    transformation_vector = np.array([image_width, image_height, image_width, image_height])
-    return bounding_boxes / transformation_vector
+    return normalize_bounding_boxes(bounding_boxes, image_size)
