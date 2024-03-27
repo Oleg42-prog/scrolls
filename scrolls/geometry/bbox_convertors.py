2621f1be057aaf90a2bfa13c1364d1c22d718605
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 16:19:55 +0300

    Add apply_linear_operator function

--- scrolls/geometry/bbox_convertors.py
+++ scrolls/geometry/bbox_convertors.py
@@ -1,10 +1,20 @@
+import numpy as np
+from scrolls.geometry.linal import apply_linear_operator
+
+
 def box_xyxy_to_xywh(bounding_box):
     bounding_boxes = bounding_box.reshape(1, -1)
     return boxes_xyxy_to_xywh(bounding_boxes)
 
 
 def boxes_xyxy_to_xywh(bounding_boxes):
-    pass
+    transformation_matrix = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [-1, 0, 1, 0],
+        [0, -1, 0, 1]
+    ])
+    return apply_linear_operator(transformation_matrix, bounding_boxes)
 
 
 def boxes_xyxy_to_xyxyn(bounding_boxes, image_size):
--- scrolls/geometry/linal.py
+++ scrolls/geometry/linal.py
@@ -5,3 +5,24 @@ def min_max_scaller(array: np.ndarray) -> np.ndarray:
     min_value = array.min()
     max_value = array.max()
     return (array - min_value) / (max_value - min_value)
+
+
+def apply_linear_operator(linear_operator, vectors):
+    """
+    Applies a linear operator to each vector in a set of vectors.
+
+    This function is based on the following theorem:
+    If A is a linear operator (represented as a square matrix), and X is a set of vectors {x1, x2, ..., xn}
+    (represented as a matrix where rows are vectors x1, x2, ..., xn), then the result of applying the operator A
+    to each vector from X can be calculated as Y = X @ A.T, where A.T is the transposed matrix A, @ is the
+    matrix multiplication operator and Y := {A @ x1, A @ x2, ..., A @ xn} by definition.
+
+    Parameters:
+    linear_operator (np.ndarray): The linear operator, represented as a square matrix.
+    vectors (np.ndarray): The set of vectors to which the operator will be applied, represented as a matrix where
+    each row is a vector.
+
+    Returns:
+    np.ndarray: The result of applying the linear operator to each vector, represented as a matrix.
+    """
+    return vectors @ linear_operator.T
