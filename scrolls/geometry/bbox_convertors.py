678a901b2701a688ee1fe8432f6cff3d94f08d48
Oleg Dudnik <oleggelo86@gmail.com>
2024-03-27 16:34:59 +0300

    Add boxes_xywh_to_xyxy

--- scrolls/geometry/bbox_convertors.py
+++ scrolls/geometry/bbox_convertors.py
@@ -17,6 +17,16 @@ def boxes_xyxy_to_xywh(bounding_boxes):
     return apply_linear_operator(transformation_matrix, bounding_boxes)
 
 
+def boxes_xywh_to_xyxy(bounding_boxes):
+    transformation_matrix = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 0, 1, 0],
+        [0, 1, 0, 1]
+    ])
+    return apply_linear_operator(transformation_matrix, bounding_boxes)
+
+
 def boxes_xyxy_to_xyxyn(bounding_boxes, image_size):
     image_width, image_height = image_size
     transformation_vector = np.array([image_width, image_height, image_width, image_height])
