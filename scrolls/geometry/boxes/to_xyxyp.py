4296698ac31e4f418ad96ca334c78e8613595ab8
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-09 13:06:14 +0300

    Add xywhn_to_xyxyp implementation

--- scrolls/geometry/boxes/to_xyxyp.py
+++ scrolls/geometry/boxes/to_xyxyp.py
@@ -1,5 +1,5 @@
 from scrolls.geometry.boxes.to_xyxy import xywh_to_xyxy, cxywh_to_xyxy
-from scrolls.geometry.boxes.to_xyxyn import xyxy_to_xyxyn, xywh_to_xyxyn
+from scrolls.geometry.boxes.to_xyxyn import xyxy_to_xyxyn, xywh_to_xyxyn, xywhn_to_xyxyn
 
 
 def xyxy_to_xyxyp(bounding_boxes, image_size):
@@ -14,8 +14,8 @@ def xyxyn_to_xyxyp(bounding_boxes):
     return bounding_boxes * 100
 
 
-def xywhn_to_xyxyp():
-    pass
+def xywhn_to_xyxyp(bounding_boxes):
+    return xywhn_to_xyxyn(bounding_boxes) * 100
 
 
 def cxywh_to_xyxyp():
