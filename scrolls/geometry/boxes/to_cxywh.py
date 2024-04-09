1502fbdf82fd2d2375fb2c23df58424d668a70a7
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-09 15:05:14 +0300

    Add xywhn_to_cxywh implementation

--- scrolls/geometry/boxes/to_cxywh.py
+++ scrolls/geometry/boxes/to_cxywh.py
@@ -2,6 +2,7 @@ import numpy as np
 from scrolls.geometry.linal import apply_linear_operator
 from scrolls.geometry.boxes.transforms import rescale_bounding_boxes
 from scrolls.geometry.boxes.to_xyxy import xyxyn_to_xyxy
+from scrolls.geometry.boxes.to_xywh import xywhn_to_xywh
 
 
 def xyxy_to_cxywh(bounding_boxes):
@@ -29,8 +30,9 @@ def xyxyn_to_cxywh(bounding_boxes, image_size):
     return xyxy_to_cxywh(xyxy)
 
 
-def xywhn_to_cxywh():
-    pass
+def xywhn_to_cxywh(bounding_boxes, image_size):
+    xywh = xywhn_to_xywh(bounding_boxes, image_size)
+    return xywh_to_cxywh(xywh)
 
 
 def cxywh_to_cxywh(bounding_boxes):
