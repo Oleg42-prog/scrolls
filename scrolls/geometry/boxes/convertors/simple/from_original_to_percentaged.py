418b609fb68fe19679c7a34d96fbbc4e0333b43b
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-11 15:59:48 +0300

    Explicit names of transformations in scrolls.geometry.boxes.convertors.simple

--- scrolls/geometry/boxes/convertors/simple/from_original_to_percentaged.py
+++ scrolls/geometry/boxes/convertors/simple/from_original_to_percentaged.py
@@ -1,19 +1,22 @@
 from scrolls.geometry.boxes.convertors.simple.from_original_to_normalized import xyxy_to_xyxyn, xywh_to_xywhn, cxywh_to_cxywhn
+from scrolls.geometry.boxes.convertors.simple.from_normalized_to_percentaged import xyxyn_to_xyxyp
+from scrolls.geometry.boxes.convertors.simple.from_normalized_to_percentaged import xywhn_to_xywhp
+from scrolls.geometry.boxes.convertors.simple.from_normalized_to_percentaged import cxywhn_to_cxywhp
 
 
 def xyxy_to_xyxyp(xyxy, original_size):
     xyxyn = xyxy_to_xyxyn(xyxy, original_size)
-    xyxyp = xyxyn * 100
+    xyxyp = xyxyn_to_xyxyp(xyxyn)
     return xyxyp
 
 
 def xywh_to_xywhp(xywh, original_size):
     xywhn = xywh_to_xywhn(xywh, original_size)
-    xywhp = xywhn * 100
+    xywhp = xywhn_to_xywhp(xywhn)
     return xywhp
 
 
 def cxywh_to_cxywhp(cxywh, original_size):
     cxywhn = cxywh_to_cxywhn(cxywh, original_size)
-    cxywhp = cxywhn * 100
+    cxywhp = cxywhn_to_cxywhp(cxywhn)
     return cxywhp
--- scrolls/geometry/boxes/convertors/simple/from_percentaged_to_original.py
+++ scrolls/geometry/boxes/convertors/simple/from_percentaged_to_original.py
@@ -1,19 +1,25 @@
-from scrolls.geometry.boxes.transforms import rescale_bounding_boxes
+from scrolls.geometry.boxes.convertors.simple.from_percentaged_to_normalized import xyxyp_to_xyxyn
+from scrolls.geometry.boxes.convertors.simple.from_percentaged_to_normalized import xywhp_to_xywhn
+from scrolls.geometry.boxes.convertors.simple.from_percentaged_to_normalized import cxywhp_to_cxywhn
+
+from scrolls.geometry.boxes.convertors.simple.from_normalized_to_original import xyxyn_to_xyxy
+from scrolls.geometry.boxes.convertors.simple.from_normalized_to_original import xywhn_to_xywh
+from scrolls.geometry.boxes.convertors.simple.from_normalized_to_original import cxywhn_to_cxywh
 
 
 def cxywhp_to_cxywh(cxywhp, original_size):
-    cxywhn = cxywhp / 100
-    cxywh = rescale_bounding_boxes(cxywhn, original_size)
+    cxywhn = cxywhp_to_cxywhn(cxywhp)
+    cxywh = cxywhn_to_cxywh(cxywhn, original_size)
     return cxywh
 
 
 def xywhp_to_xywh(xywhp, original_size):
-    xywhn = xywhp / 100
-    xywh = rescale_bounding_boxes(xywhn, original_size)
+    xywhn = xywhp_to_xywhn(xywhp)
+    xywh = xywhn_to_xywh(xywhn, original_size)
     return xywh
 
 
 def xyxyp_to_xyxy(xyxyp, original_size):
-    xyxyn = xyxyp / 100
-    xyxy = rescale_bounding_boxes(xyxyn, original_size)
+    xyxyn = xyxyp_to_xyxyn(xyxyp)
+    xyxy = xyxyn_to_xyxy(xyxyn, original_size)
     return xyxy
