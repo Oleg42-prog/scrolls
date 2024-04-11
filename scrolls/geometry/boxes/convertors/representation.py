608e37709b699f6a5b27c961fcc764173e2b3bdb
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-11 11:37:38 +0300

    Change functions order in scrolls.geometry.boxes.convertors.representation.py

--- scrolls/geometry/boxes/convertors/representation.py
+++ scrolls/geometry/boxes/convertors/representation.py
@@ -11,16 +11,6 @@ def xyxy_to_xywh(xyxy):
     return xywh
 
 
-def xywh_to_xyxy(xywh):
-    xyxy = apply_linear_operator([
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [1, 0, 1, 0],
-        [0, 1, 0, 1]
-    ], xywh)
-    return xyxy
-
-
 def xyxy_to_cxywh(xyxy):
     cxywh = apply_linear_operator([
         [0.5, 0, 0.5, 0],
@@ -29,6 +19,16 @@ def xyxy_to_cxywh(xyxy):
         [0, -1, 0, 1]
     ], xyxy)
     return cxywh
+
+
+def xywh_to_xyxy(xywh):
+    xyxy = apply_linear_operator([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 0, 1, 0],
+        [0, 1, 0, 1]
+    ], xywh)
+    return xyxy
 
 
 def xywh_to_cxywh(xywh):
