ef78ed71c60f505cb37d84bdc9ead59a2f2dd39e
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-11 14:59:18 +0300

    Add __all__ in scrolls.geometry.boxes.convertors.synonyms.__init__.py

--- scrolls/geometry/boxes/convertors/synonyms/__init__.py
+++ scrolls/geometry/boxes/convertors/synonyms/__init__.py
@@ -1,7 +1,17 @@
-from scrolls.geometry.boxes.convertors.synonyms.normalised import xyxyn_to_xywhn, xyxyn_to_cxywhn  # noqa: F401
-from scrolls.geometry.boxes.convertors.synonyms.normalised import xywhn_to_xyxyn, xywhn_to_cxywhn  # noqa: F401
-from scrolls.geometry.boxes.convertors.synonyms.normalised import cxywhn_to_xyxyn, cxywhn_to_xywhn  # noqa: F401
+from scrolls.geometry.boxes.convertors.synonyms.normalised import xyxyn_to_xywhn, xyxyn_to_cxywhn
+from scrolls.geometry.boxes.convertors.synonyms.normalised import xywhn_to_xyxyn, xywhn_to_cxywhn
+from scrolls.geometry.boxes.convertors.synonyms.normalised import cxywhn_to_xyxyn, cxywhn_to_xywhn
 
-from scrolls.geometry.boxes.convertors.synonyms.percentaged import xyxyp_to_xywhp, xyxyp_to_cxywhp  # noqa: F401
-from scrolls.geometry.boxes.convertors.synonyms.percentaged import xywhp_to_xyxyp, xywhp_to_cxywhp  # noqa: F401
-from scrolls.geometry.boxes.convertors.synonyms.percentaged import cxywhp_to_xyxyp, cxywhp_to_xywhp  # noqa: F401
+from scrolls.geometry.boxes.convertors.synonyms.percentaged import xyxyp_to_xywhp, xyxyp_to_cxywhp
+from scrolls.geometry.boxes.convertors.synonyms.percentaged import xywhp_to_xyxyp, xywhp_to_cxywhp
+from scrolls.geometry.boxes.convertors.synonyms.percentaged import cxywhp_to_xyxyp, cxywhp_to_xywhp
+
+__all__ = [
+    'xyxyn_to_xywhn', 'xyxyn_to_cxywhn',
+    'xywhn_to_xyxyn', 'xywhn_to_cxywhn',
+    'cxywhn_to_xyxyn', 'cxywhn_to_xywhn',
+
+    'xyxyp_to_xywhp', 'xyxyp_to_cxywhp',
+    'xywhp_to_xyxyp', 'xywhp_to_cxywhp',
+    'cxywhp_to_xyxyp', 'cxywhp_to_xywhp',
+]
