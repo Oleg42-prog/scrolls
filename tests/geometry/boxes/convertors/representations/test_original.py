3815c7e05b84e2a7a8b8cd6926e78c193f6adb67
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-15 19:06:59 +0300

    📝 (test_original.py): Refactor test classes to use a base class for shared setup and assertions to reduce code duplication and improve maintainability modified:   tests/geometry/boxes/convertors/representations/test_original.py

--- tests/geometry/boxes/convertors/representations/test_original.py
+++ tests/geometry/boxes/convertors/representations/test_original.py
@@ -6,61 +6,80 @@ from scrolls.geometry.boxes.convertors.representations.original import cxywh_to_
 from tests.geometry.boxes.convertors import ground_truth as gt
 
 
-class TestBoxConvertorsOriginalRepresentations(unittest.TestCase):
+class TestBoxConvertorsOriginalRepresentationsBase(unittest.TestCase):
 
     def setUp(self):
         self.eps = 1e-6
-        self.left = gt.LEFT_BIRD
-        self.right = gt.RIGHT_BIRD
-        self.union = gt.UNION_BIRDS
+        self.gt = gt.UNION_BIRDS
 
     def test_xyxy_to_xywh(self):
-        xywh_gt = self.union['xywh']
+        xywh_gt = self.gt['xywh']
 
-        xyxy = self.union['xyxy']
+        xyxy = self.gt['xyxy']
         xywh = xyxy_to_xywh(xyxy)
 
         np.testing.assert_allclose(xywh, xywh_gt, rtol=self.eps, atol=self.eps)
 
     def test_xyxy_to_cxywh(self):
-        cxywh_gt = self.union['cxywh']
+        cxywh_gt = self.gt['cxywh']
 
-        xyxy = self.union['xyxy']
+        xyxy = self.gt['xyxy']
         cxywh = xyxy_to_cxywh(xyxy)
 
         np.testing.assert_allclose(cxywh, cxywh_gt, rtol=self.eps, atol=self.eps)
 
     def test_xywh_to_xyxy(self):
-        xyxy_gt = self.union['xyxy']
+        xyxy_gt = self.gt['xyxy']
 
-        xywh = self.union['xywh']
+        xywh = self.gt['xywh']
         xyxy = xywh_to_xyxy(xywh)
 
         np.testing.assert_allclose(xyxy, xyxy_gt, rtol=self.eps, atol=self.eps)
 
     def test_xywh_to_cxywh(self):
-        cxywh_gt = self.union['cxywh']
+        cxywh_gt = self.gt['cxywh']
 
-        xywh = self.union['xywh']
+        xywh = self.gt['xywh']
         cxywh = xywh_to_cxywh(xywh)
 
         np.testing.assert_allclose(cxywh, cxywh_gt, rtol=self.eps, atol=self.eps)
 
     def test_cxywh_to_xyxy(self):
-        xyxy_gt = self.union['xyxy']
+        xyxy_gt = self.gt['xyxy']
 
-        cxywh = self.union['cxywh']
+        cxywh = self.gt['cxywh']
         xyxy = cxywh_to_xyxy(cxywh)
 
         np.testing.assert_allclose(xyxy, xyxy_gt, rtol=self.eps, atol=self.eps)
 
     def test_cxywh_to_xywh(self):
-        xywh_gt = self.union['xywh']
+        xywh_gt = self.gt['xywh']
 
-        cxywh = self.union['cxywh']
+        cxywh = self.gt['cxywh']
         xywh = cxywh_to_xywh(cxywh)
 
         np.testing.assert_allclose(xywh, xywh_gt, rtol=self.eps, atol=self.eps)
+
+
+class TestBoxConvertorsOriginalRepresentationsLeft(TestBoxConvertorsOriginalRepresentationsBase):
+
+    def setUp(self):
+        self.eps = 1e-6
+        self.gt = gt.LEFT_BIRD
+
+
+class TestBoxConvertorsOriginalRepresentationsRight(TestBoxConvertorsOriginalRepresentationsBase):
+
+    def setUp(self):
+        self.eps = 1e-6
+        self.gt = gt.RIGHT_BIRD
+
+
+class TestBoxConvertorsOriginalRepresentationsUnion(TestBoxConvertorsOriginalRepresentationsBase):
+
+    def setUp(self):
+        self.eps = 1e-6
+        self.gt = gt.UNION_BIRDS
 
 
 if __name__ == '__main__':
