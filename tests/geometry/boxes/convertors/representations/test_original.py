45bec91f1650e335b3cdecdc8eddcdc58da406be
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-16 11:18:12 +0300

    ✨ (test_original.py): Remove unnecessary local variables modified:   tests/geometry/boxes/convertors/representations/test_original.py

--- tests/geometry/boxes/convertors/representations/test_original.py
+++ tests/geometry/boxes/convertors/representations/test_original.py
@@ -14,40 +14,28 @@ class TestBoxConvertorsOriginalRepresentationsBase(unittest.TestCase):
         self.gt = gt.UNION_BIRDS
 
     def test_xyxy_to_xywh(self):
-        xywh_gt = self.gt.xywh
-        xyxy = self.gt.xyxy
-        xywh = xyxy_to_xywh(xyxy)
-        np.testing.assert_allclose(xywh, xywh_gt, rtol=self.EPS, atol=self.EPS)
+        xywh = xyxy_to_xywh(self.gt.xyxy)
+        np.testing.assert_allclose(xywh, self.gt.xywh, rtol=self.EPS, atol=self.EPS)
 
     def test_xyxy_to_cxywh(self):
-        cxywh_gt = self.gt.cxywh
-        xyxy = self.gt.xyxy
-        cxywh = xyxy_to_cxywh(xyxy)
-        np.testing.assert_allclose(cxywh, cxywh_gt, rtol=self.EPS, atol=self.EPS)
+        cxywh = xyxy_to_cxywh(self.gt.xyxy)
+        np.testing.assert_allclose(cxywh, self.gt.cxywh, rtol=self.EPS, atol=self.EPS)
 
     def test_xywh_to_xyxy(self):
-        xyxy_gt = self.gt.xyxy
-        xywh = self.gt.xywh
-        xyxy = xywh_to_xyxy(xywh)
-        np.testing.assert_allclose(xyxy, xyxy_gt, rtol=self.EPS, atol=self.EPS)
+        xyxy = xywh_to_xyxy(self.gt.xywh)
+        np.testing.assert_allclose(xyxy, self.gt.xyxy, rtol=self.EPS, atol=self.EPS)
 
     def test_xywh_to_cxywh(self):
-        cxywh_gt = self.gt.cxywh
-        xywh = self.gt.xywh
-        cxywh = xywh_to_cxywh(xywh)
-        np.testing.assert_allclose(cxywh, cxywh_gt, rtol=self.EPS, atol=self.EPS)
+        cxywh = xywh_to_cxywh(self.gt.xywh)
+        np.testing.assert_allclose(cxywh, self.gt.cxywh, rtol=self.EPS, atol=self.EPS)
 
     def test_cxywh_to_xyxy(self):
-        xyxy_gt = self.gt.xyxy
-        cxywh = self.gt.cxywh
-        xyxy = cxywh_to_xyxy(cxywh)
-        np.testing.assert_allclose(xyxy, xyxy_gt, rtol=self.EPS, atol=self.EPS)
+        xyxy = cxywh_to_xyxy(self.gt.cxywh)
+        np.testing.assert_allclose(xyxy, self.gt.xyxy, rtol=self.EPS, atol=self.EPS)
 
     def test_cxywh_to_xywh(self):
-        xywh_gt = self.gt.xywh
-        cxywh = self.gt.cxywh
-        xywh = cxywh_to_xywh(cxywh)
-        np.testing.assert_allclose(xywh, xywh_gt, rtol=self.EPS, atol=self.EPS)
+        xywh = cxywh_to_xywh(self.gt.cxywh)
+        np.testing.assert_allclose(xywh, self.gt.xywh, rtol=self.EPS, atol=self.EPS)
 
 
 class TestBoxConvertorsOriginalRepresentationsLeft(TestBoxConvertorsOriginalRepresentationsBase):
