7f88a33aa2c5f666647f77691a2e5658fc952e86
Oleg Dudnik <oleggelo86@gmail.com>
2024-04-16 11:14:25 +0300

    ✨ (ground_truth.py): Refactor ground_truth.py to use dataclasses for BoxRepresentations 📝 (test_original.py): Update test_original.py to use attributes of BoxRepresentations class modified:   tests/geometry/boxes/convertors/ground_truth.py modified:   tests/geometry/boxes/convertors/representations/test_original.py

--- tests/geometry/boxes/convertors/ground_truth.py
+++ tests/geometry/boxes/convertors/ground_truth.py
@@ -1,20 +1,27 @@
+from dataclasses import dataclass
 import numpy as np
 
+@dataclass
+class BoxRepresentations:
+    xyxy: np.array
+    xywh: np.array
+    cxywh: np.array
 
-LEFT_BIRD = {
-    'xyxy': np.array([151, 62, 386, 527]),
-    'xywh': np.array([151, 62, 235, 465]),
-    'cxywh': np.array([268.5, 294.5, 235, 465])
-}
 
-RIGHT_BIRD = {
-    'xyxy': np.array([430, 88, 962, 503]),
-    'xywh': np.array([430, 88, 532, 415]),
-    'cxywh': np.array([696, 295.5, 532, 415])
-}
+LEFT_BIRD = BoxRepresentations(
+    xyxy=np.array([151, 62, 386, 527]),
+    xywh=np.array([151, 62, 235, 465]),
+    cxywh=np.array([268.5, 294.5, 235, 465])
+)
 
-UNION_BIRDS = {
-    'xyxy': np.vstack([LEFT_BIRD['xyxy'], RIGHT_BIRD['xyxy']]),
-    'xywh': np.vstack([LEFT_BIRD['xywh'], RIGHT_BIRD['xywh']]),
-    'cxywh': np.vstack([LEFT_BIRD['cxywh'], RIGHT_BIRD['cxywh']])
-}
+RIGHT_BIRD = BoxRepresentations(
+    xyxy=np.array([430, 88, 962, 503]),
+    xywh=np.array([430, 88, 532, 415]),
+    cxywh=np.array([696, 295.5, 532, 415])
+)
+
+UNION_BIRDS = BoxRepresentations(
+    xyxy=np.vstack([LEFT_BIRD.xyxy, RIGHT_BIRD.xyxy]),
+    xywh=np.vstack([LEFT_BIRD.xywh, RIGHT_BIRD.xywh]),
+    cxywh=np.vstack([LEFT_BIRD.cxywh, RIGHT_BIRD.cxywh])
+)
--- tests/geometry/boxes/convertors/representations/test_original.py
+++ tests/geometry/boxes/convertors/representations/test_original.py
@@ -14,51 +14,39 @@ class TestBoxConvertorsOriginalRepresentationsBase(unittest.TestCase):
         self.gt = gt.UNION_BIRDS
 
     def test_xyxy_to_xywh(self):
-        xywh_gt = self.gt['xywh']
-
-        xyxy = self.gt['xyxy']
+        xywh_gt = self.gt.xywh
+        xyxy = self.gt.xyxy
         xywh = xyxy_to_xywh(xyxy)
-
         np.testing.assert_allclose(xywh, xywh_gt, rtol=self.EPS, atol=self.EPS)
 
     def test_xyxy_to_cxywh(self):
-        cxywh_gt = self.gt['cxywh']
-
-        xyxy = self.gt['xyxy']
+        cxywh_gt = self.gt.cxywh
+        xyxy = self.gt.xyxy
         cxywh = xyxy_to_cxywh(xyxy)
-
         np.testing.assert_allclose(cxywh, cxywh_gt, rtol=self.EPS, atol=self.EPS)
 
     def test_xywh_to_xyxy(self):
-        xyxy_gt = self.gt['xyxy']
-
-        xywh = self.gt['xywh']
+        xyxy_gt = self.gt.xyxy
+        xywh = self.gt.xywh
         xyxy = xywh_to_xyxy(xywh)
-
         np.testing.assert_allclose(xyxy, xyxy_gt, rtol=self.EPS, atol=self.EPS)
 
     def test_xywh_to_cxywh(self):
-        cxywh_gt = self.gt['cxywh']
-
-        xywh = self.gt['xywh']
+        cxywh_gt = self.gt.cxywh
+        xywh = self.gt.xywh
         cxywh = xywh_to_cxywh(xywh)
-
         np.testing.assert_allclose(cxywh, cxywh_gt, rtol=self.EPS, atol=self.EPS)
 
     def test_cxywh_to_xyxy(self):
-        xyxy_gt = self.gt['xyxy']
-
-        cxywh = self.gt['cxywh']
+        xyxy_gt = self.gt.xyxy
+        cxywh = self.gt.cxywh
         xyxy = cxywh_to_xyxy(cxywh)
-
         np.testing.assert_allclose(xyxy, xyxy_gt, rtol=self.EPS, atol=self.EPS)
 
     def test_cxywh_to_xywh(self):
-        xywh_gt = self.gt['xywh']
-
-        cxywh = self.gt['cxywh']
+        xywh_gt = self.gt.xywh
+        cxywh = self.gt.cxywh
         xywh = cxywh_to_xywh(cxywh)
-
         np.testing.assert_allclose(xywh, xywh_gt, rtol=self.EPS, atol=self.EPS)
 
 
